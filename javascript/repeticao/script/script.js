6d4396d567a9d9804e780818e45c7c90a2b6aada
ricardofigueiredocamargo <116184098+ricardofigueiredocamargo@users.noreply.github.com>
2023-04-07 10:45:21 -0300

    testes

---
+++ javascript/repeticao/script/script.js
@@ -0,0 +1,33 @@
+// WHILE
+/*
+var x = 1
+
+document.write('Início <br>')
+
+while (x <= 10) {
+
+    x++
+
+    if (x % 2 == 1) {
+        continue
+    }
+
+    document.write(x + '<br>')
+}
+
+document.write('Fim <br>')
+*/
+
+// DO WHILE 
+/*
+var x = 11
+
+do {
+    document.write(x + '<br>')
+} while (x <= 10)
+*/
+
+// FOR 
+for (var x = 10; x >= 0; x--) {
+    document.write(x + '<br>')
+}
