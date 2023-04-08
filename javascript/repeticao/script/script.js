17978900cd10d0dfdd95953f49febb7a31d155a0
ricardofigueiredocamargo <116184098+ricardofigueiredocamargo@users.noreply.github.com>
2023-04-08 11:49:17 -0300

    testes

--- javascript/repeticao/script/script.js
+++ javascript/repeticao/script/script.js
@@ -1,4 +1,4 @@
-// WHILE
+// WHILE ------------------------------------
 /*
 var x = 1
 
@@ -18,7 +18,7 @@ while (x <= 10) {
 document.write('Fim <br>')
 */
 
-// DO WHILE 
+// DO WHILE --------------------------------
 /*
 var x = 11
 
@@ -27,7 +27,49 @@ do {
 } while (x <= 10)
 */
 
-// FOR 
+// FOR ------------------------------------
+/*
 for (var x = 10; x >= 0; x--) {
     document.write(x + '<br>')
 }
+*/
+
+// TABUADA ---------------------------------
+/*
+for (let y = 1; y <= 10; y++) {
+    for (let x = 1; x <= 10; x++) {
+        document.write(`${y} x ${x} = ${y*x} <br>`)
+    }
+    document.write('<br>')
+}
+*/
+
+// FOR IN ----------------------------------
+/*
+var listaConvidados = []
+
+listaConvidados['a'] = 'Jorge'
+listaConvidados[10] = 'Jamilton'
+listaConvidados['zebra'] = 'José'
+listaConvidados[-1] = 'Ana'
+listaConvidados[true] = 'Maria'
+
+for (let pos in listaConvidados) {
+    document.write(listaConvidados[pos] + '<br>')
+}
+*/
+
+// FOR EACH -----------------------------------
+
+var listaFuncionarios = ['Viviane', 'Rosangela', 'Miguel', 'Lucas', 'Fernanda']
+
+document.write(listaFuncionarios + '<br>')
+document.write('<br>')
+listaFuncionarios.forEach(function(valor, indice, array) {
+    document.write(`valor: ${valor} | índice: ${indice} <br>`)
+    if (valor == 'Lucas') {
+        array[indice] = 'Um novo valor'
+    }
+})
+
+document.write(listaFuncionarios[3])
